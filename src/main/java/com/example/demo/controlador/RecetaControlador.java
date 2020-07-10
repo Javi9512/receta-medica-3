45f7db3905370cf916c785258f30fa29953a1ddf
Javi <javier.michay95@ucuenca.edu.ec>
2020-07-09 21:52:26 -0500

    Buscar receta por id

--- src/main/java/com/example/demo/controlador/RecetaControlador.java
+++ src/main/java/com/example/demo/controlador/RecetaControlador.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +41,14 @@ public class RecetaControlador {
 		Receta crearReceta=servicioReceta.createReceta(receta);
 		return ResponseEntity.status(HttpStatus.CREATED).body(crearReceta);
 		
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Receta> getById(@PathVariable("id") Long id){
+		Receta receta=servicioReceta.getReceta(id);
+		if(receta==null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(receta);
 	}
 
 }
